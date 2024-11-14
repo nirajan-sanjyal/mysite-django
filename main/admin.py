74666c6ebbb1071a4048835706ee0cdc73e7c23b
nirajan-sanjyal <sanjyal@gmail.com>
2024-11-14 16:06:22 +0545

    addheading

--- main/admin.py
+++ main/admin.py
@@ -3,4 +3,12 @@ from django.contrib import admin
 from main.models import Tutorial
 
 # Register your models here.
-admin.site.register(Tutorial)
+class TutorialAdmin(admin.ModelAdmin):
+    
+    fieldsets=[
+        ("Title", {"fields": ["tutorial_title"]}),
+        ("Content", {"fields":["tutorial_content"]}),
+        ("date", {"fields": ["tutorial_published"]})
+    ]
+    list_display= ['tutorial_published', 'tutorial_title', 'tutorial_content']
+admin.site.register(Tutorial, TutorialAdmin)
